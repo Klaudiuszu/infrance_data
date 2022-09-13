d6e717893f058c99d85b6250c1c7071bfc9a301a
Klaudiuszu <klaudiuszpupiec.kontakt@gmail.com>
2022-09-13 23:52:52 +0200

    clear array from duplicate

--- src/container/Header/Header.jsx
+++ src/container/Header/Header.jsx
@@ -1,8 +1,6 @@
 import { useEffect, useMemo, useState } from "react";
 import useForm from "./useForm";
-import axios from "axios";// używam do tworzenia zapytań w przeglądarce
-import { useTranslation } from "react-i18next";
-import i18next from "i18next";
+import axios from "axios"; // używam do tworzenia zapytań w przeglądarce
 
 import "./Header.scss";
 
@@ -11,92 +9,94 @@ const Header = () => {
   const [offset, setOffset] = useState(0); // jeśli chciałbym użyć paginacji dla bazy zacząłbym od tych dwóch stanów - w tym przypadku nie jest to konieczne
   const [data, setData] = useState([]);
   const [btnState, setBtnState] = useState(false);
+  const [nationality, setNationality] = useState([]);
+  const [message, setMessage] = useState('');
+  const [detail, setDetail] = useState({ name: "" });
+  const [checkNames, setCheckNames] = useState(() => {
+    const initialValue = []; 
+    const namesJson = localStorage.getItem('itemList'); // pobieram dane jeśli istnieją w lockal storage
+    if(namesJson !== null) {
+      return JSON.parse(namesJson); // warunek, jeśli tablica coś zawiera zwróć dane do pustej tablicy
+    }
 
-  const [error, setError] = useState(null);
-
-  //const [value, handleChange] = useForm();
-
-  const [detail, setDetail] = useState({name: ""});
-  const [name, setName] = useState("");
-
-  console.log(detail)
+  });
   
+  console.log(checkNames);
+
+  const unique = checkNames.filter((value, index) => {
+    return checkNames.indexOf(value) === index;
+  });
+
+  console.log(unique)
+
+  
+  
+
   const handleChange = (e) => {
-    const {name, value} = e.target;
+    
+    const { name, value } = e.target;
+
+    const result = value.replace(/[^a-z]/gi, '');
+
+    setMessage(result);
+
     setDetail((prev) => {
-    return { ...prev, [name]: value};
+      return { ...prev, [name]: value };
     });
   };
 
-console.log(data)
+  useEffect(() => {
+    localStorage.setItem('itemList', JSON.stringify(unique)); // tworze lockal Storage w przeglądarce
+  },[checkNames]);
 
-const handleSubmit = (e) => {
-  e.preventDefault();
-}
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    fetch(`https://api.genderize.io?name=${detail.name}`)
+      .then((response) => response.json())
+      .then((data) => {
+        setData(data);
+        const str = data.name;
+        setCheckNames(arr => [...arr, str])
+      })
+      .catch((err) => {
+        console.log(err.message);
+      });
+
+  };
 
 
-  
-  const { t } = useTranslation();
-  
-//   useEffect(() => {
-//     const fetchData = async () => {
-//       const result = await axios(
-//         `https://api.genderize.io?name=${items}`,
-//       );
-//       setData(result.data);
-//     };
-//     fetchData();
-// },[]);
-
-   useEffect(() => {
-      fetch(`https://api.genderize.io?name=${detail.name}`)
-         .then((response) => response.json())
-         .then((data) => {
-            console.log(data);
-            setData(data);
-          })
-         .catch((err) => {
-            console.log(err.message);
-         });
-   }, []);
-
-const addName = () => {
-
-  setBtnState((btnState) => !btnState);
-}
-
+  const switcher = () => {
+    setBtnState((btnState) => !btnState);
+  };
 
 
   return (
     <div className="app__Header">
-      <form 
-        onSubmit={handleSubmit}
-        >
-          <label>
-            <h1>{t("translation_test")}</h1>
-            <input 
-            type="text"
-            name="name"
-            onChange={handleChange}
-            />
-          </label>
-        <button 
-        onClick={addName}
-        className="main-button"
-        type="submit"
-        >{t("check")}
+      <form onSubmit={handleSubmit}>
+        <label>
+          <h1>test</h1>
+          <input type="text" name="name" onChange={handleChange} value={message}
+          />
+        </label>
+        <button onClick={switcher} className="main-button" type="submit">
+        check
         </button>
       </form>
-      <div 
-      className="app__Header-result"
-      style={{ visibility: btnState ? "visible" : "hidden" }}
+      <div
+        className="app__Header-result"
+        style={{ visibility: btnState ? "visible" : "hidden" }}
       >
         <div className="app__Header-wrapper">
-          <h2>{t("translation_test")}</h2>
           <h1>{data.name}</h1>
-          <h1>{data.gender}</h1>
+          <h2>{data.gender}</h2>
+          <button
+          onClick={switcher}
+          >close</button>
         </div>
       </div>
+          {checkNames.map((item) => (
+          <p className="app__Header-storage">{item}</p>
+          ))}
     </div>
   );
 };
